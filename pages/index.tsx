b7561485007b7d587c30597b1295b0369e4d8fbb
Jelani Harris <mayanobsidian@gmail.com>
2022-08-19 21:35:18 -0500

    Added global style and changed main page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="text-2xl">Hello World</div>
+    <div className="flex flex-col h-screen justify-center items-center">
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl">
+        <div className="text-2xl text-black">Hello World</div>
+      </div>
+    </div>
   )
 }
 
